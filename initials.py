2e17c9ace418879851b571f18cad3f43d48fc6ee
Borketey Bortey Joshua <2425403913@live.gctu.edu.gh>
2026-05-30 22:19:57 +0000

    Sumitted initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Joshua Bortey Borketey
+# STUDENT ID: 2425403913
+# Email: 2425403913@live.gctu.edu.gh
+# Grader: Joshua
+#
+# On my honor, Joshua Bortey Borketey  this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints styled initials
 
+    print()
+    print("...JBB")
+    print()
+
+    print("...JJJJJJJJJJJJ......BBBBBBBBBB........BBBBBBBBBB...")
+    print("........JJ...........BB......BBB.......BB......BBB..")
+    print("........JJ...........BB.......BB.......BB.......BB..")
+    print("........JJ...........BB......BBB.......BB......BBB..")
+    print("........JJ...........BBBBBBBBBB........BBBBBBBBBB...")
+    print("........JJ...........BBBBBBBBBB........BBBBBBBBBB...")
+    print("........JJ...........BB......BBB.......BB......BBB..")
+    print("...J....JJ...........BB.......BB.......BB.......BB..")
+    print("...JJJJJJJ...**......BB......BBB...**..BB......BBB..")
+    print("....JJJJJ....**......BBBBBBBBBB....**..BBBBBBBBBB...")
+
+    print()
 
 
 main()
